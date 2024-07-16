417c1ebe2024fce6ffef8aa7ed796717fb90bfa7
melikesoygullucu <melikesoygul@hotmail.com>
2024-07-16 15:50:30 +0300

    Picker and CIImage conversion added

--- ImageRecognition/ViewController.swift
+++ ImageRecognition/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var chosenImage = CIImage()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
-
+    @IBAction func changeButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        
+        if let ciImage = CIImage(image: imageView.image!){
+            chosenImage = ciImage
+        }
+        
+        recognizeImage(image: chosenImage)
+    }
+    
+    func recognizeImage(image: CIImage) {
+        // 1. Request
+        // 2. Handler
+    }
 }
 
